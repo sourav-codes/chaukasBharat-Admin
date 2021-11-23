eebf61a09f071641e4178f5e4d4f3ecfd7571c99
sourav-codes <singhsourav201998@gmail.com>
2021-11-23 15:47:56 +0530

    changes

--- src/components/post_create.js
+++ src/components/post_create.js
@@ -13,32 +13,40 @@ import {
     TextInput,
 } from "react-admin";
 
-export const PostList = (props) => (
+export const PostList = props => (
     <List {...props}>
-    <SimpleForm>
-    <TextInput disabled source="id" />
-        <TextInput source="title" />
-        <TextInput multiline source="body" />
-    </SimpleForm>
-</List>   
+       <Datagrid>
+        <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+          <EditButton />
+        </Datagrid>
+    </List>
 );
 
-export const PostEdit = (props) => (
+export const PostEdit = props => (
     <Edit {...props}>
-    <SimpleForm>
-    <TextInput disabled source="id" />
-        <TextInput source="title" />
-        <TextInput multiline source="body" />
-    </SimpleForm>
-</Edit>   
+        <SimpleForm>
+          <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+               <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+         <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
 );
 
-export const PostCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-        <TextInput disabled source="id" />
-            <TextInput source="title" />
-            <TextInput multiline source="body" />
-        </SimpleForm>
-    </Create>
-);
+export const PostCreate = props => (
+       <Create {...props}>
+            <SimpleForm>
+                <ReferenceInput source="userId" reference="users">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <TextInput source="title" />
+                <TextInput multiline source="body" />
+            </SimpleForm>
+        </Create>
+        );
